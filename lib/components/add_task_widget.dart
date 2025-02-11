90c7c698c3a5617b9f1b534045ce96b4ff47c372
Melkizedeki <mmagota2@gmail.com>
2025-02-11 11:51:33 +0300

    Dealing with the layout od todo app towards statemanagements

--- lib/components/add_task_widget.dart
+++ lib/components/add_task_widget.dart
@@ -29,7 +29,19 @@ class BottomTaskAdd extends StatelessWidget {
               hintStyle: TextStyle(
                 fontSize: 15,
               ),
-              border: OutlineInputBorder(),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.lightBlueAccent,
+                  width: 1.0,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.lightBlueAccent,
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                ),
+              ),
             ),
           ),
           SizedBox(
